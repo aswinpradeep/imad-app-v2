b0a8ad49ac2e653a45216b70d8ea191182d3ad9d
aswinpradeep <aswinpradeep1@gmail.com>
2017-02-14 23:07:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,112 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var article1=
+{
+    title:'article1',
+    date:'feb 14,2017',
+    heading:'article 1 h3',
+    content:
+ `
+    <p>
+                
+                p1A! content A! content A! content A! content A! content  A! content A! content A! content A! content A! content  A! content A! content A! content A! content A! content
+                 A! content A! content A! content A! content A! content  A! content A! content A! content A! content A! content  A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content
+                 
+                  A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content
+                
+                
+            </p>
+            <p>
+               p2 A! content A! content A! content A! content A! content  A! content A! content A! content A! content A! content  A! content A! content A! content A! content A! content
+                 A! content A! content A! content A! content A! content  A! content A! content A! content A! content A! content  A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content
+                 
+                  A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content
+                
+            </p>
+            
+            <p>
+               p3 A! content A! content A! content A! content A! content  A! content A! content A! content A! content A! content  A! content A! content A! content A! content A! content
+                 A! content A! content A! content A! content A! content  A! content A! content A! content A! content A! content  A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content
+                 
+                  A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content A! content
+                
+                
+                
+            </p> 
+
+`
+
+
+
+
+
+};
+
+
+function createtemplate(data)
+{
+    
+ var title=data.title;
+ var heading=data.heading;
+ var date=data.date;
+ var content=data.content;
+    
+
+var HtmlTemplate=
+
+`
+<html>
+    <head>
+        <title>
+            
+            ${title}
+        </title>
+        
+        <meta name="viewport" content="width=device-width,initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+        
+       
+        
+    </head>
+    <body>
+        
+    <div class="container">
+        
+        <div>
+            <a href="/">home</a>
+            
+        </div>
+        <hr/>
+        
+        <h3>
+            
+            ${heading}
+        </h3>
+        
+        <div>
+            ${date}
+        </div>
+        
+        <div>
+           ${content}
+        </div>
+        
+</div>        
+    
+    </body>
+    
+</html>
+
+`
+;
+return HtmlTemplate;
+
+
+}
+
+
+
 app.get('/', function (req, res) 
   {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -15,7 +121,7 @@ app.get('/', function (req, res)
 app.get('/article1',function(req,res)
   {
     
-      res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+      res.send(createtemplate(article1));
   }
 );
 app.get('/article2',function(req,res)
